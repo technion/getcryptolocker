85a677301078792239485b5557ee380578f55347
Technion <technion@lolware.net>
2018-03-28 09:55:07 +0000

    Remove cache flags, done frontend now.

--- push.sh
+++ push.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/sh
-aws s3 sync --profile cryptolocker_website --region us-east-1 _site/ s3://get-cryptolocker --delete --cache-control="max-age=3153600" --exclude "*.html"
-aws s3 sync --profile cryptolocker_website --region us-east-1 _site/ s3://get-cryptolocker --delete --cache-control="max-age=86400" --exclude "*" --include "*.html"
+aws s3 sync --profile cryptolocker_website --region us-east-1 _site/ s3://get-cryptolocker --delete 
 
